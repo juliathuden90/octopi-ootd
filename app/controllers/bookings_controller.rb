7b4a22dbc1caeb727e9c7a48c580343f21c8ec44
Adlina Basuki <ad.basuki@gmail.com>
2023-03-01 16:15:27 +0800

    fixed changes in new bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,25 @@
 class BookingsController < ApplicationController
-  before_action :set_user, only: [:create]
-  before_action :set_booking, only: [:create]
+  # before_action :set_booking, only: [:create]
+  before_action :set_squid, only: [:new, :create]
 
   def index
-  end
-
-  def create
+    @bookings = Booking.all
   end
 
   def new
     @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.squid = @squid
+    @booking.status = "pending"
+    if @booking.save
+      redirect_to bookings_path
+    else
+      redirect_to new_squid_booking_path(@squid)
+    end
   end
 
   def update
@@ -30,9 +40,5 @@ class BookingsController < ApplicationController
 
   def booking_params
     params.require(:booking).permit(:rent_date)
-  end
-
-  def set_user
-    @user = current_user
   end
 end
